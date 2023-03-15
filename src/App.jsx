e2c004f6819ebe6b099eb6a33007d65d194e58a6
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-15 20:28:55 +0100

    layout: add App content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Header from "./components/Header";
+import StatsCardLarge from "./components/StatsCardLarge";
+import StatsCardSmall from "./components/StatsCardSmall";
+import { followerStats, otherStats } from "./constants";
+import { ThemeProvider } from "styled-components";
+import {
+  darkTheme,
+  lightTheme,
+  GlobalStyle,
+  Container,
+  GridContainer,
+} from "./globalStyles.js";
+import { useState } from "react";
 
 function App() {
-  return (
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme = () => {
+    setTheme((current) => (current === "dark" ? "light" : "dark"));
+  };
 
-  )
+  return (
+    <>
+      <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+        <GridContainer>
+          {followerStats.map((stats) => {
+            return <StatsCardLarge key={stats.id} stats={stats} />;
+          })}
+        </GridContainer>
+        <Container>
+          <h2>Overview - Today</h2>
+          <GridContainer>
+            {otherStats.map((stats) => {
+              return <StatsCardSmall key={stats.id} stats={stats} />;
+            })}
+          </GridContainer>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
