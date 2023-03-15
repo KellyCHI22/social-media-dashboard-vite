b35485d25a0c6839c54a564d830b93fbcfc3688d
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-15 20:21:38 +0100

    feat: add constants

---
+++ src/constants/index.js
@@ -0,0 +1,89 @@
+export const followerStats = [
+  {
+    id: 1,
+    social: "facebook",
+    account: "@nathanf",
+    count: 1987,
+    change: 12,
+  },
+  {
+    id: 2,
+    social: "twitter",
+    account: "@nathanf",
+    count: 1044,
+    change: 99,
+  },
+  {
+    id: 3,
+    social: "instagram",
+    account: "@realnathanf",
+    count: 11000,
+    change: 1099,
+  },
+  {
+    id: 4,
+    social: "youtube",
+    account: "Nathan F.",
+    count: 8239,
+    change: -144,
+  },
+];
+
+export const otherStats = [
+  {
+    id: 1,
+    title: "Page Views",
+    social: "facebook",
+    count: 87,
+    change: 3,
+  },
+  {
+    id: 2,
+    title: "Likes",
+    social: "facebook",
+    count: 52,
+    change: 2,
+  },
+  {
+    id: 3,
+    title: "Likes",
+    social: "instagram",
+    count: 5462,
+    change: 2257,
+  },
+  {
+    id: 4,
+    title: "Profile Views",
+    social: "instagram",
+    count: 52000,
+    change: 1375,
+  },
+  {
+    id: 5,
+    title: "Retweets",
+    social: "twitter",
+    count: 117,
+    change: 303,
+  },
+  {
+    id: 6,
+    title: "Likes",
+    social: "twitter",
+    count: 507,
+    change: 553,
+  },
+  {
+    id: 7,
+    title: "Likes",
+    social: "youtube",
+    count: 107,
+    change: -19,
+  },
+  {
+    id: 8,
+    title: "Total Views",
+    social: "youtube",
+    count: 1407,
+    change: -12,
+  },
+];
